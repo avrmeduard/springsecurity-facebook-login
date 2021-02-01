5ac554205866a539b45a9d1244a57366477a5972
avrm <avrmeduard@gmail.com>
2021-02-01 19:24:57 +0200

    Initial commit

---
+++ src/main/java/com/avrm/springsecurityfacebooklogin/SpringsecurityFacebookLoginApplication.java
@@ -0,0 +1,13 @@
+package com.avrm.springsecurityfacebooklogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityFacebookLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecurityFacebookLoginApplication.class, args);
+	}
+
+}
