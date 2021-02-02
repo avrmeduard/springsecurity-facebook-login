7967fd79ad4a9b9fb1654b91d4ed42f242397577
avrm <avrmeduard@gmail.com>
2021-02-02 20:15:20 +0200

    + annotation added

--- src/main/java/com/avrm/springsecurityfacebooklogin/SpringsecurityFacebookLoginApplication.java
+++ src/main/java/com/avrm/springsecurityfacebooklogin/SpringsecurityFacebookLoginApplication.java
@@ -2,8 +2,10 @@ package com.avrm.springsecurityfacebooklogin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
 @SpringBootApplication
+@EnableOAuth2Sso
 public class SpringsecurityFacebookLoginApplication {
 
 	public static void main(String[] args) {
